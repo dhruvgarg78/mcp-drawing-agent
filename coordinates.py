9812fe8769fba5d546e75b96974448eafab04379
Dhruv Garg <dhruvg7864@gmail.com>
2025-05-08 02:00:17 -0500

    completed, removed extra

--- coordinates.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-import time
-time.sleep(2)  # Give you 5 seconds to switch to the Paint window
-print(pyautogui.position())  # Move your mouse to top-left of Paint canvas
-
-#rect on paint: Point(x=660, y=98)
